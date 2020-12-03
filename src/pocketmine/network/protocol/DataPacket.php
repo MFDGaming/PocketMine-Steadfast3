b6cea90dd0fe81be4d045c63943636284c93600b
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-03 08:57:11 +0200

    Update DataPacket.php

--- src/pocketmine/network/protocol/DataPacket.php
+++ src/pocketmine/network/protocol/DataPacket.php
@@ -122,6 +122,21 @@ abstract class DataPacket extends BinaryStream{
 		self::$packetsIds[Info::PROTOCOL_406] = $oClass->getConstants();		
 		self::$packetsIds[Info::PROTOCOL_407] = $oClass->getConstants();
 		self::$packetsIds[Info::PROTOCOL_408] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_409] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_410] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_411] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_412] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_413] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_414] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_415] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_416] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_417] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_418] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_419] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_420] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_421] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_422] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_423] = $oClass->getConstants();
 	}
 	
 }

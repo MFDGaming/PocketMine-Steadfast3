747683724048ec77a8bc9de65dfb3f02613b23cd
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-18 08:58:07 +0200

    Update AcknowledgePacket.php

--- src/raklib/protocol/AcknowledgePacket.php
+++ src/raklib/protocol/AcknowledgePacket.php
@@ -22,76 +22,71 @@ use raklib\Binary;
 #include <rules/RakLibPacket.h>
 
 abstract class AcknowledgePacket extends Packet{
-    /** @var int[] */
-    public $packets = [];
+	/** @var int[] */
+	public $packets = [];
 
-    public function encode(){
-        parent::encode();
-        $payload = "";
-        sort($this->packets, SORT_NUMERIC);
-        $count = count($this->packets);
-        $records = 0;
+	public function encode(){
+		parent::encode();
+		$records = 0;
+		$payload = "";
+		sort($this->packets, SORT_NUMERIC);
+		if (count($this->packets) > 0) {
+			$start = $this->packets[0];
+			$last = $this->packets[0];
+			for ($pointer = 1; $pointer < count(self.packets); $pointer++) {
+				$current = $this->packets[$pointer];
+				$diff = $current - $last;
+				if ($diff == 1) {
+					$last = $current;
+				} else if ($diff > 1) {
+					if ($start == $last) {
+						$payload .= "\x01";
+						$payload .= Binary::writeLTriad($start);
+						$start = $last = $current;
+					} else {
+						$payload .= "\x00";
+						$payload .= Binary::writeLTriad($start);
+						$payload .= Binary::writeLTriad($last);
+						$start = $last = $current;
+					}
+					$records++;
+				}
+			}
+			if ($start == $last) {
+				$payload .= "\x01";
+				$payload .= Binary::writeLTriad($start);
+			} else {
+				$payload .= "\x00";
+				$payload .= Binary::writeLTriad($start);
+				$payload .= Binary::writeLTriad($last);
+			}
+			$records++;
+		}
+		$this->putShort($records);
+		$this->put($payload);
+	}
 
-        if($count > 0){
-            $pointer = 1;
-            $start = $this->packets[0];
-            $last = $this->packets[0];
-
-            while($pointer < $count){
-                $current = $this->packets[$pointer++];
-                $diff = $current - $last;
-                if($diff === 1){
-                    $last = $current;
-                }elseif($diff > 1){ //Forget about duplicated packets (bad queues?)
-                    if($start === $last){
-                        $payload .= "\x01";
-                        $payload .= Binary::writeLTriad($start);
-                        $start = $last = $current;
-                    }else{
-                        $payload .= "\x00";
-                        $payload .= Binary::writeLTriad($start);
-                        $payload .= Binary::writeLTriad($last);
-                        $start = $last = $current;
-                    }
-                    ++$records;
-                }
-            }
-
-            if($start === $last){
-                $payload .= "\x01";
-                $payload .= Binary::writeLTriad($start);
-            }else{
-                $payload .= "\x00";
-                $payload .= Binary::writeLTriad($start);
-                $payload .= Binary::writeLTriad($last);
-            }
-            ++$records;
-        }
-
-        $this->putShort($records);
-        $this->buffer .= $payload;
-    }
-
-    public function decode(){
-        parent::decode();
-        $count = $this->getShort();
-        $this->packets = [];
-        $cnt = 0;
-        for($i = 0; $i < $count and !$this->feof() and $cnt < 4096; ++$i){
-            if($this->getByte() === 0){
-                $start = $this->getLTriad();
-                $end = $this->getLTriad();
-                if(($end - $start) > 512){
-                    $end = $start + 512;
-                }
-                for($c = $start; $c <= $end; ++$c){
-                    $this->packets[$cnt++] = $c;
-                }
-            }else{
-                $this->packets[$cnt++] = $this->getLTriad();
-            }
-        }
-    }
+	public function decode(){
+		parent::decode();
+		$this->packets = [];
+		$recordCount = $this->getShort();
+		for ($i = 0; $i < $recordCount; $i++) {
+			$recordType = $this->getByte();
+			if ($recordType == 0) {
+				$start = $this->getLTriad();
+				$end = $this->getLTriad();
+				for ($packet = $start; $packet < $end + 1; $packet++) {
+					$this->packets[] = $packet;
+					if (count($this->packets) > 4096) {
+						return;
+					}
+				}
+			}else{
+				$packet = $this->getLTriad();
+				$this->packets[] = $packet;
+			}
+		}
+	}
 
 	public function clean(){
 		$this->packets = [];
